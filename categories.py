24ef8c2747103b9ecd1a9ca5e44ec542ee0484d7
danish0313 <danishoo.dk@gmail.com>
2026-03-17 17:25:52 +0100

    ai_service and gameround

--- categories.py
+++ categories.py
@@ -9,13 +9,18 @@ class Categories:
     def get_all_categories(self):
         return self.categories
 
+    def get_all_summaries(self):
+        summaries = []
+        for item in self.categories:
+            summaries.append(item.get_summary())
+        return summaries
 
 
-category1 = Category("Flowering plant")
+
+"""category1 = Category("Flowering plant")
 #category2 = Category("List of European countries")
 category2 = Category("List of music artists and bands from England")
-category3 = Category("List of domesticated animals","https://en.wikipedia.org/wiki/List_of_domesticated_animals")
-
+category3 = Category("List of music artists and bands from England")
 
 
 total_categories = Categories([category1, category2, category3])
@@ -24,6 +29,6 @@ all_items = total_categories.get_all_categories()
 
 for item in all_items:
     if item:
-        print(item.get_summary())
+        print(item.get_summary())"""
 
 
--- game/game_round.py
+++ game/game_round.py
@@ -1,22 +1,75 @@
-# import random
+import random
 
-class GameRound:
-    def __init__(self, title: str, true_statement: str, false_statement: str):
-        """
-        set true and false statement of game round
-        set shuffle logic
-        """
-        pass
+def get_quiz_statement():
+    #you need to get question from storage location
+    dummy_true_or_false = [
+        {
+            "true_statement": "APG III system of flowering plant classification is the third version of a modern, mostly molecular-based system of plant taxonomy being developed by the Angiosperm Phylogeny Group (APG).",
+            "false_statement": "Acorus is a genus of dicot flowering plants."
+        },
 
-    def display(self, round_number: int):
-        # Prints the round number and both statements to the console.
-        pass
+        {
+            "true_statement": "APG III system of flowering plant classification is the third version of a modern, mostly molecular-based system of plant taxonomy being developed by the Angiosperm Phylogeny Group (APG).",
+            "false_statement": "Acorus is a genus of dicot flowering plants."
+        },
 
-    def get_player_answer(self):
-        # Prompts the player to enter A or B, validates input, and
-        #         prints immediate feedback.
-        pass
+        {
+            "true_statement": "APG III system of flowering plant classification is the third version of a modern, mostly molecular-based system of plant taxonomy being developed by the Angiosperm Phylogeny Group (APG).",
+            "false_statement": "Acorus is a genus of dicot flowering plants."
+        },
+        {
+            "true_statement": "APG III system of flowering plant classification is the third version of a modern, mostly molecular-based system of plant taxonomy being developed by the Angiosperm Phylogeny Group (APG).",
+            "false_statement": "Acorus is a genus of dicot flowering plants."
+        }
 
-    def _shuffle_options(self):
-        # Randomly assigns statements to option A and option B.
-        pass
+    ] #quiz dict
+
+    return dummy_true_or_false
+
+
+def play_round():
+    quiz_statements = get_quiz_statement()
+    player_score = 0
+    for i, item in enumerate(quiz_statements, start=1):
+        #get the statements
+        correct_answer = item["true_statement"]
+        wrong_answer = item["false_statement"]
+
+      # randomised and assigned statements
+        option_dict = assign_randomised_statements(correct_answer, wrong_answer)
+
+        print(f"Round {i}, which one is False?")
+        for letter, statement in option_dict.items():
+            print(f" Choice {letter}: {statement}")
+
+        user_answer = get_player_answer()
+        if option_dict[user_answer] == wrong_answer:
+            print("correct, you have one point")
+            player_score += 1
+        else:
+            print("incorrect")
+
+    end_game(player_score)
+
+
+def assign_randomised_statements(true_answer, false_answer):
+    options = [true_answer, false_answer]
+    random.shuffle(options)
+    letter_choices = ["a", "b"]
+    option_dict = dict(zip(letter_choices, options)) #zip combines the answers with the letter choices
+    return option_dict
+
+def get_player_answer():
+    answer = input("choose the false statement: ")
+    return answer
+
+
+def end_game(score):
+    if score >= 3:
+        print("Great job. Go to the next round!")
+    else:
+        print("AI fooled you. Better luck next time")
+#need to add the replay button
+
+
+print(play_round())
