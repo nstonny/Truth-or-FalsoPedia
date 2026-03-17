4210dbb0489b2dbb9b7e55dae68dcf70117da984
Hasireddy <malikireddy.uma@gmail.com>
2026-03-17 10:41:09 +0100

    feat:Get data from Wikipedia

--- data_wikipedia.py
+++ data_wikipedia.py
@@ -1,2 +1,28 @@
-def  get_data_wikipedia(url):
-    pass
+from bs4 import BeautifulSoup
+import requests
+
+def get_data_wikipedia(url):
+        """Extracting table data from the url"""
+
+        headers = {
+            "User-Agent": "Mozilla/5.0"
+        }
+
+        res = requests.get(url, headers=headers)
+        soup = BeautifulSoup(res.content, "html.parser")
+
+        tables = soup.find_all("table", class_="wikitable")
+        data = []
+
+        for table in tables:
+            rows = table.find_all("tr")
+
+            for row in rows[1:]:
+                cols = row.find_all("td")
+                if cols:
+                    text = cols[0].get_text(strip=True)  # produces Domestic dog (Canis familiaris)[3]
+                    if text:
+                        text = text.split("(")[
+                            0]  # produces ['Domestic dog ', 'Canis familiaris)[3]'] [0] keeps Domestic Dog
+                        data.append(text.strip())
+        return data
